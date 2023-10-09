4a1115948f39fd9a07d2b749bc62e8c352db0a5f
StoneHee99 <kiit0901@gmail.com>
2023-10-09 17:10:47 +0900

    Feat: ResumeView 클래스 추가(#4)
    사용자로부터 데이터를 입력받는 뷰 클래스 생성
    
    close #4

---
+++ src/main/java/kr/kernel/pretask/view/ResumeView.java
@@ -0,0 +1,11 @@
+package kr.kernel.pretask.view;
+
+import java.util.Scanner;
+
+public class ResumeView {
+    private Scanner s;
+
+    public ResumeView() {
+        s = new Scanner(System.in);
+    }
+}
